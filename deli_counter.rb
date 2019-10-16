12eb952f9ab433300ab84a2f0b535b89e80aa917
Wendy Harris <wendyv510@hotmail.com>
2019-10-16 18:12:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,9 @@ def take_a_number (katz_deli, "new customer")
   puts "Welcome #{new customer}, You are number #{index} in line."
 end 
 
-new customer = "Ada" 
+take_a_number(katz_deli, "Ada")
+
+take_a_number(katz_deli, "Grace")
+
+take_a_number(katz_deli, "Kent") 
   
