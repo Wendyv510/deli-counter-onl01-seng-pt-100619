fcbc17575ae4d9f93e5e36c085ca7c93c68b085e
Wendy Harris <wendyv510@hotmail.com>
2019-10-16 18:01:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line (number_of_people)
+  if number_of_people == 0 
+    puts "The line is currently empty."
+  else number_of_people > 0 
+    puts "There are #{number_of_people} in line."
+  end 
+  
