e8de3177359a76afe450ec885429ca9de59ff535
Wendy Harris <wendyv510@hotmail.com>
2019-10-16 18:33:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,6 @@
+katz_deli=[]
+
+
 def line (number_of_people)
   if number_of_people == 0 
     puts "The line is currently empty."
@@ -10,20 +13,44 @@ def take_a_number (katz_deli,new customer)
   katz_deli << "new customer"
   puts "Welcome #{new customer}, You are number #{index} in line."
 end 
-
-take_a_number(katz_deli,"Ada")
-
-take_a_number(katz_deli,"Grace")
-
-take_a_number(katz_deli,"Kent") 
   
   
   def line 
-    katz_deli=["Ada", "Grace", "Kent"]
+    katz_deli=[]
     katz_deli.each do 
       
       puts "The line is currently #{katz_deli.each.with.index}."
       
     end 
+    
+  def now_serving 
+    puts "Currently serving #{katz_deli[1]}."
+    katz_deli.shift
+  end 
+  
+  
+  take_a_number(katz_deli,"Ada")
+  
+  take_a_number(katz_deli,"Grace")
+  
+  take_a_number(katz_deli,"Kent")
+  
+  line(katz_deli)
+  
+  now_serving(katz_deli)
+  
+  line(katz_deli)
+  
+  take_a_number(katz_deli,"Matz")
+  
+  line(katz_deli)
+  
+  now_serving(katz_deli)
+  
+  line(katz_deli)
+    
+    
+    
+    
     
     
